ee935c734e7fc92d3b5bfa6b4049ad6db076cc54
[ryan larson] <[rylarso@gmail.com]>
2022-06-20 12:48:48 -0600

    api stuff

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
-console.log("Hey there");
-console.log("I am so glad this works.");
-console.log("How about a git test?");
+//initiate the express server
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+//pull in db
+const persist = require("./persist");
+
+//put in command line flags
+const flags = require("flags");
+flags.defineNumber("port", 3000, "Ports for the http server to listen");
+flags.parse();
+
+//put in env vars
+const dotenv = require("dotenv");
+
+//set up port number
+const port = flags.get("port") || process.env.PORT || 3000;
+
+//set up server paths and handlers
+app.get("/todo/:id", (req, res) => {
+    const id = req.params.id;
+    const todo = persist.getTodo(id);
+    res.json(todo);
+});
+
+app.post("/todo", (req, res) => {
+    const id = req.params.id;
+    const todo = persist.getTodo(id);
+    persist.addTodo(req.body);
+    res.send(req.body);
+});
+
+app.delete("/todo/:id", (req, res) => {
+    const id = req.params.id;
+    const todo = persist.removeId(id);
+    res.json(todo);
+});
+
+//sends entire object with a change
+app.put("/todo/:id", (req, res) => {
+    const id = req.params.id;
+    const todo = persist.getTodo(id);
+    res.json(todo);
+});
+
+//sends part of an object with a change
+app.patch("/todo/:id", (req, res) => {
+    const id = req.params.id;
+    const todo = persist.getTodo(id);
+});
+
+//start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
---
+++ persist/index.js
@@ -0,0 +1,53 @@
+const todo_db = {};
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+
+const addTodo = function (todo) {
+    id = makeid(8);
+    username = "ryan"
+    password = "this is a password"
+    todo.id = id;
+    todo.username = username;
+    todo.password = password;
+    todo_db[username] = todo;
+    console.log(todo_db);
+    return todo;
+};
+
+const getTodo = function (id) {
+    return todo_db[id];
+};
+
+const removeId = function (id) {
+    delete todo_db[id];
+    return todo_db;
+};
+
+const setTodo = function (id, todo) {
+    todo_db[id] = todo;
+    return todo;
+};
+
+const patchTodo = function (id, todoData) {
+    for (const key in todoData) {
+        todo_db[id][key] = todoData[key];
+    }
+    const todo = todo_db[id];
+    return todo;
+};
+
+module.exports = {
+    addTodo: addTodo,
+    getTodo: getTodo,
+    removeId: removeId,
+    setTodo: setTodo,
+    patchTodo: patchTodo,
+};
